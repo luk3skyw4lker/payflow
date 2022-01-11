b924ee862e12f5014e61874980ac03e1b4b2b2f1
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2022-01-11 15:57:33 -0300

    [feat]: Add app_colors file config

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Comuns
+  static const primary = Color(0xFFFF941A);
+  static const delete = Color(0xFFE83F5B);
+  static const grey = Color(0xFF585666);
+
+  // Texto
+  static const heading = Color(0xFF585666);
+  static const body = Color(0xFF706E7A);
+
+  // Items
+  static const background = Color(0xFFFFFFFF);
+  static const stroke = Color(0xFFE3E3E6);
+  static const shape = Color(0xFFFAFAFC);
+}
